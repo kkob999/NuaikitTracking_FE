295b7c679704e92eb87a5dfc1075187b009daef4
KKob <kob.ruji@gmail.com>
2024-03-01 14:44:38 +0700

    ยีห้

---
+++ .next/static/chunks/_error.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0375dbda174580c440ff26acdc442a377cc2741b13fed56b1c3df3cc3239dc76
+size 3931
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2c6ddd45a6f2af1d716767c48fb11f53e63ef9ee523806018bb49ce864034869
+oid sha256:a897974dff4bc36ecbdc2d009db1ca18214191f0baf916bd879156597a6c9581
 size 47935
