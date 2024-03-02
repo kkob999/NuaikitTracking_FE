593a6e26ff3ba6db92e131a56d61c5d4905c27c3
gigiftt <gift.natcha@q-chang.com>
2024-03-02 16:02:12 +0700

    conflict

--- .next/static/chunks/pages/TermView.js
+++ .next/static/chunks/pages/TermView.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:014d208d3869aeecf0ca1be09f19e6ae2cff9aa09f3ac351a01460c18ec2b237
-size 39207481
+oid sha256:951cfb1d73c8ad4fd19d1a429780de16b5d0aef3e63ff3ee444a1db6aa708740
+size 39244056
---
+++ .next/static/webpack/pages/TermView.9f5d3f3426b7b5a5.hot-update.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c35b9ea17cf40b5d73fd210fc74d88fa672ba2cf26de1629cca940fd33e541e7
+size 132357
